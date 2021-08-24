be493bc66bcdecfd6d99f55b6d014fe65638340e
sallyyjan <sallyyujeongan@gmail.com>
2021-08-24 10:06:10 -0700

    VideoCapture setup

--- disney_wand.py
+++ disney_wand.py
@@ -2,3 +2,18 @@
 import numpy as np
 import cv2
 
+# output screen dimensions
+frameWidth = 600
+framHeight = 400
+# output brightness
+brightness = 150
+
+# capture video from default camera (webcam)
+vid = cv2.VideoCapture(index=0)
+# set frame dimensions
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth)
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, framHeight)
+# set image brightness
+vid.set(cv2.CAP_PROP_BRIGHTNESS, brightness)
+
+# 
